f971d935daa0d7e216fcec013ce3d0391ede3868
TJ Chapley <tj@chapley.com>
2025-02-01 23:21:22 -0500

    Lots of CSS updates and new API callsw

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ const people = {
 };
 let data = null;
 
+function createRow(headerText, cellText) {
+	let tr = document.createElement("tr");
+	let header = document.createElement("th");
+	header.setAttribute("scope", "row");
+	header.textContent = headerText; 
+	let cell = document.createElement("td");
+	cell.textContent = cellText
+	tr.append(header);
+	tr.append(cell);
+	return tr
+}
+
 async function load() {
 	if (localStorage.getItem('data') !== null) {
 		console.log("Pulling from LocalStorage");
@@ -29,11 +41,21 @@ async function load() {
 	document.getElementById("lastupdated").innerHTML = new Date(data.lastUpdated);
 }
 
+async function grabActivities(bill) {
+	const response = await fetch(`https://api.congress.gov/v3/bill/119/${bill["Bill Type"].toLowerCase()}/${bill["Bill Number"]}/actions?format=json&api_key=hKFfR1PceFsoUsZBZ70sE3YfOEbnHrIxbWah4036`);
+	return response.json();
+}
+
+async function grabText(bill) {
+	const response = await fetch(`https://api.congress.gov/v3/bill/119/${bill["Bill Type"].toLowerCase()}/${bill["Bill Number"]}/text?format=json&api_key=hKFfR1PceFsoUsZBZ70sE3YfOEbnHrIxbWah4036`);
+	return response.json();
+}
+
 async function createPage() {
 	let nav = document.getElementById("mainNav");
 	let main = document.getElementById("main");
 
-	Object.keys(people).forEach(function(id) {
+	for await (id of Object.keys(people)) {
 		let person = people[id];
 		let link = document.createElement("a");
 		link.setAttribute("href", `#${person}`);
@@ -42,20 +64,89 @@ async function createPage() {
 
 		let div = document.createElement("div");
 		div.setAttribute("id", `${person}`);
+		div.setAttribute("class", "person");
 		let header = document.createElement("h3");
 		header.textContent = person;
 		div.append(header);
 		let bills = data["bills"]
 			.filter(item => item.CongressPerson === person)
 			.sort((a, b) => new Date(b["Introduced Date"]) - new Date(a["Introduced Date"]));
-		bills.forEach(function(bill) {
-			let text = document.createElement("p");
-			text.textContent = JSON.stringify(bill);	
-			div.append(text);
-		});
+		for await (const bill of bills) {
+			let date = new Date(bill["Introduced Date"]);
+			let newdate = (date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear();
+
+			
+			let button = document.createElement("button");
+			button.setAttribute("class", "collapsible");
+			button.textContent = `${bill["Bill Type"]}${bill["Bill Number"]}`;
+			let content = document.createElement("div");
+			content.setAttribute("class", "content");
+			let table = document.createElement("table");
+			let tbody = document.createElement("tbody");
+			tbody.append(createRow("Title:", bill["Title"]));
+			tbody.append(createRow("Sponsor Type:", `${bill["SponsorType"]} (Introduced ${newdate})`));
+			
+			let tr = document.createElement("tr");
+			let header = document.createElement("th");
+			header.setAttribute("scope", "row");
+			header.textContent = "Activities:"; 
+			let cell = document.createElement("td");
+			let activities = await grabActivities(bill);
+			for await (const action of activities["actions"]) {
+				let li = document.createElement("li");
+				li.textContent = `${action["text"]} - (${action["actionDate"]})`;
+				cell.append(li);
+			}
+
+			tr.append(header);
+			tr.append(cell);
+			tbody.append(tr);
+
+			tr = document.createElement("tr");
+			header = document.createElement("th");
+			header.setAttribute("scope", "row");
+			header.textContent = "Text:"; 
+			cell = document.createElement("td");
+			let anchor = document.createElement("a");
+			anchor.textContent = "No Text Yet";
+			let billText = await grabText(bill);
+			if (billText["textVersions"].lenght > 0) {
+				anchor.setAttribute("class", "billText");
+				anchor.setAttribute("href", billText["textVersions"][0]["formats"][0]["url"]);
+				anchor.textContent = billText["textVersions"][0]["formats"][0]["url"];
+			}
+			cell.append(anchor);
+
+			tr.append(header);
+			tr.append(cell);
+			tbody.append(tr);
+			
+			table.append(tbody);
+			content.append(table);
+			div.append(button);
+			div.append(content);
+		
+
+
+		}
 		main.append(div);
 
-	});
+	}
+
+	let coll = document.getElementsByClassName("collapsible");
+
+	for (let i = 0; i < coll.length; i++) {
+		coll[i].addEventListener("click", function() {
+                        this.classList.toggle("active");
+                        let content = this.nextElementSibling;
+                        if (content.style.maxHeight) {
+                                content.style.maxHeight = null;
+                        } else {
+                                content.style.maxHeight = content.scrollHeight + "px";
+                        }
+                });
+
+	}
 	
 }
 
